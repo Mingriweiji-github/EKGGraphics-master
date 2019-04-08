dbbdb99c101c9efc36102be8f8f488af917b6b33
gsf <gsf@gsfdeMacBook-Air.local>
2019-04-08 17:34:48 +0800

    prase hex data

--- EKGGraphics-master/ViewController.m
+++ EKGGraphics-master/ViewController.m
@@ -37,7 +37,16 @@
     self.view.backgroundColor = [UIColor blackColor];
     
 //    [self readData];
-    self.ctrlManager = [[CBCentralManager alloc] initWithDelegate:self queue:dispatch_get_main_queue()];
+    
+//    self.ctrlManager = [[CBCentralManager alloc] initWithDelegate:self queue:dispatch_get_main_queue()];
+    
+    NSString *content = @"aaaa01a0 000d000a 000b000a 000b000b 000a000b 000c000c 0009000c 0007000c 000a000d 0009000c 000a000d 000a000c 000d000c 000d000b 000a000a 000e000a 000b000d 000c000b 000a000b 0007000b 000c000c 000c000b 000c000b 000d000b 000a000b 000a000b 000d000b 000e000b 000e000b 000a000a 000d000b 000c000b 000b000b 000b000d 000c0009 000b000b 000e000a 000d0009 000d";
+    //    NSString *content = [NSString stringWithFormat:@"%@",characteristic.value];
+    NSString *temp = [content stringByReplacingOccurrencesOfString:@" " withString:@""];
+    NSLog(@"temp=%@",temp);
+    NSString *lbeString = [temp substringWithRange:NSMakeRange(8, 300)];//前8位-2个16进制数对应设备信息不处理
+    NSArray *tempData = [self praseHexWithContentString:lbeString withRatio:1];
+    NSLog(@"tempArr = %@",tempData);
 }
 - (void)createWorkDataSourceWithTimeInterval:(NSTimeInterval )timeInterval
 {
@@ -79,7 +88,7 @@
             @autoreleasepool {
                 NSString *lbeString = [line substringWithRange:NSMakeRange(8, 300)];//前8位-2个16进制数对应设备信息不处理
                 [lbeString enumerateSubstringsInRange:NSMakeRange(0, lbeString.length) options:NSStringEnumerationByWords usingBlock:^(NSString * _Nullable substring, NSRange substringRange, NSRange enclosingRange, BOOL * _Nonnull stop) {
-                    NSArray *tempData = [self insertWhiteSpaceWithNum:substring];
+                    NSArray *tempData = [self praseHexWithContentString:substring withRatio:3];
                     NSLog(@"tempData=%@",tempData);
                     [self.mArr addObjectsFromArray:tempData];
                 }];
@@ -130,7 +139,7 @@
  @param string HexString
  @return short 数组
  */
--(NSArray *)insertWhiteSpaceWithNum:(NSString *)string{
+- (NSArray *)praseHexWithContentString:(NSString *)string withRatio:(int)ratio{
     NSString *tempString = string;
     NSInteger size = tempString.length / 4;
     NSMutableArray *tempArr = [NSMutableArray array];
@@ -139,7 +148,7 @@
         NSData *tempData = [self dataFromHexString:hexString];
         unsigned num = [self parseIntFromData:tempData];
         //int ->short
-        short shortNum = ((short)num) / 3;
+        short shortNum = ((short)num) / ratio;
         
         [tempArr addObject:[NSString stringWithFormat:@"%d",shortNum]];
     }
@@ -272,15 +281,17 @@
         }
     }
 }
+#pragma mark - 获取的l外设charateristic的值
 /**
- //获取的charateristic的值
+ !注意，value的类型是NSData，具体开发时，会根据外设协议制定的方式去解析数据
  */
 - (void)peripheral:(CBPeripheral *)peripheral didUpdateValueForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error{
-    //!注意，value的类型是NSData，具体开发时，会根据外设协议制定的方式去解析数据
     NSLog(@"characteristic uuid:%@  value:%@",characteristic.UUID,characteristic.value);
-    unsigned numberInt = [self parseIntFromData:characteristic.value];
-    short shortNum = ((short)numberInt) / 3;
-    NSLog(@"shortNum=%d",shortNum);
+    /*NSData
+     value:<aaaa01a0 000d000a 000b000a 000b000b 000a000b 000c000c 0009000c 0007000c 000a000d 0009000c 000a000d 000a000c 000d000c 000d000b 000a000a 000e000a 000b000d 000c000b 000a000b 0007000b 000c000c 000c000b 000c000b 000d000b 000a000b 000a000b 000d000b 000e000b 000e000b 000a000a 000d000b 000c000b 000b000b 000b000d 000c0009 000b000b 000e000a 000d0009 000d>
+     */
+    
+    
 }
 //搜索到Characteristic的Descriptors
 -(void)peripheral:(CBPeripheral *)peripheral didDiscoverDescriptorsForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error{
